8955942f819ec01c084863d9fe974600ccfcd596
Michael Walczyk <mwalczyk2@gmail.com>
2017-03-28 09:08:42 -0700

    Finished linear regression. Nothing working yet.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 extern crate csv;
 
-/// Performs stochastic gradient descent
+/// http://mccormickml.com/2014/03/04/gradient-descent-derivation/
+///
+/// Performs one step of stochastic gradient descent
 fn gradient_descent(m: f64,
                     b: f64,
                     points_x: &[f64],
                     points_y: &[f64],
-                    learning_rate: f64,
-                    iterations: u32) -> (f64, f64) {
-    (0.0, 0.0)
+                    learning_rate: f64) -> (f64, f64) {
+    // Initialize both partial derivatives to zero
+    let mut m_partial = 0.0;
+    let mut b_partial = 0.0;
+    let n = points_x.len() as f64;
+
+    // Compute partial derivatives with respect to m and b, respectively
+    for (x, y) in points_x.iter().zip(points_y.iter()) {
+        m_partial += -(2.0 / n) * (y - (m * x + b));
+        b_partial += (2.0 / n) * x * (y - (m * x + b));
+    }
+
+    // Update m and b
+    (m - learning_rate * m_partial, b - learning_rate * b_partial)
 }
 
 /// Computes the mean squared error
@@ -34,16 +47,20 @@ fn run(points_x: &[f64], points_y: &[f64]) {
     let m_initial = 0.0;
     let b_initial = 0.0;
     let learning_rate = 0.0001;
-    let iterations = 1000;
+    let iterations = 1;
 
     // (3) Train the model
     let error_initial = mse(m_initial, b_initial, points_x, points_y);
     println!("Starting gradient descent...");
     println!("Initially, m = {}, b = {}, error = {}", m_initial, b_initial, error_initial);
-    let (m_final, b_final) = gradient_descent(m_initial, b_initial, points_x, points_y, learning_rate, iterations);
+    let mut m_current = m_initial;
+    let mut b_current = b_initial;
+    for i in 0..iterations {
+        (m_current, b_current) = gradient_descent(m_initial, b_initial, points_x, points_y, learning_rate);
+    }
 
-    let error_final = mse(m_final, b_final, points_x, points_y);
-    println!("After {} iterations,  m = {}, b = {}, error = {}", iterations, m_final, b_final, error_final);
+    let error_final = mse(m_current, b_current, points_x, points_y);
+    println!("After {} iterations,  m = {}, b = {}, error = {}", iterations, m_current, b_current, error_final);
 }
 
 fn main() {
