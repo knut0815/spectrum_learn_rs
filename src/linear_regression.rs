0faac778f63b521651cd2261026b3e653b425807
Michael Walczyk <mwalczyk2@gmail.com>
2017-03-28 23:28:49 -0700

    Working on small refactor.

---
+++ src/linear_regression.rs
@@ -0,0 +1,102 @@
+pub struct Line {
+    pub m: f64,
+    pub b: f64
+}
+
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+pub struct LinearRegression<'a> {
+    pub points: &'a [(f64, f64)],
+    learning_rate: f64,
+    iterations: u32,
+}
+
+impl<'a> LinearRegression<'a> {
+    /// Constructs a new `LinearRegression` instance.
+    pub fn from_records(pts: &'a [(f64, f64)]) -> LinearRegression<'a> {
+        LinearRegression {
+            points: pts,
+            learning_rate: 0.0001,
+            iterations: 10,
+        }
+    }
+
+    /// Sets the learning rate that will be used during gradient
+    /// descent.
+    ///
+    /// By default, the learning rate is set to 0.0001.
+    pub fn learning_rate(mut self, rate: f64) -> LinearRegression<'a> {
+        self.learning_rate = rate;
+        self
+    }
+
+    /// Sets the number of iterations that will be performed during
+    /// gradient descent.
+    ///
+    /// By default, 10 iterations are performed.
+    pub fn iterations(mut self, iters: u32) -> LinearRegression<'a> {
+        self.iterations = iters;
+        self
+    }
+
+    /// Performs one iteration of the stochastic gradient descent
+    /// algorithm, given a starting slope `m` and y-intercept `b`.
+    ///
+    /// Returns the newly calculated slope and y-intercept as a tuple.
+    fn gradient_descent(&self, m: f64, b: f64) -> (f64, f64) {
+        let mut m_current = m;
+        let mut b_current = b;
+        let n = self.points.len() as f64;
+        let inv_n = 1.0 / n;
+
+        for _ in 0..self.iterations {
+            // Compute the partial derivatives of the MSE cost function with
+            // respect to m and b
+            let mut m_partial = 0.0;
+            let mut b_partial = 0.0;
+            for &(x, y) in self.points.iter() {
+                m_partial += ((m_current * x + b_current) - y) * x;
+                b_partial += (m_current * x + b_current) - y;
+            }
+            m_partial *= inv_n;
+            b_partial *= inv_n;
+
+            // Update the current estimates of m and b: the gradient points in
+            // the direction of increase, which is why we subtract it from the
+            // current estimates
+            m_current -= self.learning_rate * m_partial;
+            b_current -= self.learning_rate * b_partial;
+        }
+        (m_current, b_current)
+    }
+
+    /// Computes the mean squared error, given the current slope
+    /// `m` and y-intercept `b`.
+    fn mse(&self, m: f64, b: f64) -> f64 {
+        let mut calculated_error = 0.0;
+
+        for &(x, y) in self.points.iter() {
+            // The equation of the current regression line is: y = m * x + b
+            // Find the squared distance between the point on the line corresponding
+            // to x and the actual value of y
+            calculated_error += (y - (m * x + b)).powf(2.0);
+        }
+        calculated_error / self.points.len() as f64
+    }
+
+    /// Runs the linear regression algorithm with starting
+    /// slope m and y-intercept b.
+    pub fn run(&self, m_initial: f64, b_initial: f64) {
+        let error_initial = self.mse(m_initial, b_initial);
+        println!("Starting gradient descent...");
+        println!("Initially, m = {}, b = {}, error = {}", m_initial, b_initial, error_initial);
+
+        let (m_final, b_final) = self.gradient_descent(m_initial, b_initial);
+        let error_final = self.mse(m_final, b_final);
+
+        println!("After {} iterations,  m = {}, b = {}, error = {}", self.iterations, m_final, b_final, error_final);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,46 @@
 extern crate csv;
 extern crate gl;
 extern crate glutin;
-extern crate libc;
 
-/// http://mccormickml.com/2014/03/04/gradient-descent-derivation/
-///
-/// Performs one step of stochastic gradient descent
-fn gradient_descent(m: f64,
-                    b: f64,
-                    points_x: &[f64],
-                    points_y: &[f64],
-                    learning_rate: f64,
-                    iterations: u32) -> (f64, f64) {
-    let mut m_current = m;
-    let mut b_current = b;
-    let n = points_x.len() as f64;
-    let inv_n = 1.0 / n;
+mod linear_regression;
 
-    // Perform gradient descent i times
-    for i in 0..iterations {
-        // Compute the partial derivatives of the MSE cost function with
-        // respect to m and b
-        let mut m_partial = 0.0;
-        let mut b_partial = 0.0;
-        for (x, y) in points_x.iter().zip(points_y.iter()) {
-            m_partial += ((m_current * x + b_current) - y) * x;
-            b_partial += ((m_current * x + b_current) - y);
-        }
-        m_partial *= inv_n;
-        b_partial *= inv_n;
-
-        // Update the current estimates of m and b: the gradient points in
-        // the direction of increase, which is why we subtract it from the
-        // current estimates
-        m_current -= learning_rate * m_partial;
-        b_current -= learning_rate * b_partial;
-    }
-        (m_current, b_current)
-}
-
-/// Computes the mean squared error
-fn mse(m: f64,
-       b: f64,
-       points_x: &[f64],
-       points_y: &[f64]) -> f64 {
-    let mut calculated_error = 0.0;
-
-    for (x, y) in points_x.iter().zip(points_y.iter()) {
-        // The equation of the current regression line is: y = m * x + b
-        // Find the squared distance between the point on the line corresponding
-        // to x and the actual value of y
-        calculated_error += (y - (m * x + b)).powf(2.0);
-    }
-    calculated_error / points_x.len() as f64
-}
-
-fn run(points_x: &[f64], points_y: &[f64]) {
-    // (1) Collect data
-
-    // (2) Define hyperparameters
-    let m_initial = 0.0;
-    let b_initial = 0.0;
-    let learning_rate = 0.0001;
-    let iterations = 1000;
-
-    // (3) Train the model
-    let error_initial = mse(m_initial, b_initial, points_x, points_y);
-    println!("Starting gradient descent...");
-    println!("Initially, m = {}, b = {}, error = {}", m_initial, b_initial, error_initial);
-
-    let (m_final, b_final) = gradient_descent(m_initial, b_initial, points_x, points_y, learning_rate, iterations);
-    let error_final = mse(m_final, b_final, points_x, points_y);
-
-    println!("After {} iterations,  m = {}, b = {}, error = {}", iterations, m_final, b_final, error_final);
-}
+use linear_regression::LinearRegression;
 
 fn main() {
     let mut reader = csv::Reader::from_file("./src/data.csv").unwrap().has_headers(false);
 
-    let mut v0: Vec<f64> = Vec::new();
-    let mut v1: Vec<f64> = Vec::new();
-
+    let mut points: Vec<(f64, f64)> = Vec::new();
     for record in reader.decode() {
         // Test score, number of hours studied
-        let (c0, c1): (f64, f64) = record.unwrap();
-        v0.push(c0);
-        v1.push(c1);
+        let p: (f64, f64) = record.unwrap();
+        points.push(p);
     }
 
-    run(&v0, &v1);
+    let lin_reg = LinearRegression::from_records(&points).iterations(3);
+    lin_reg.run(0.0, 0.0);
+
+    // OpenGL drawing
+    let window = glutin::Window::new().unwrap();
+    window.set_inner_size(200, 200);
+
+    let mut vao = 0;
+    unsafe {
+        window.make_current();
+        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+        gl::ClearColor(1.0, 0.0, 0.0, 1.0);
+        let mut vao = 0;
+        let mut vbo = 0;
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+    }
+    for event in window.wait_events() {
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+        window.swap_buffers();
+
+        match event {
+            glutin::Event::Closed => break,
+            _ => ()
+        }
+    }
 }
