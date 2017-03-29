a57d869a295ed6594c9e431385157d6ea2bda42d
Michael Walczyk <michael.walczyk@obscuradigital.com>
2017-03-28 17:30:37 -0800

    Working on integrating gl-rs and glutin crates.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate csv;
+extern crate gl;
+extern crate glutin;
+extern crate libc;
 
 /// http://mccormickml.com/2014/03/04/gradient-descent-derivation/
 ///
@@ -7,20 +10,33 @@ fn gradient_descent(m: f64,
                     b: f64,
                     points_x: &[f64],
                     points_y: &[f64],
-                    learning_rate: f64) -> (f64, f64) {
-    // Initialize both partial derivatives to zero
-    let mut m_partial = 0.0;
-    let mut b_partial = 0.0;
+                    learning_rate: f64,
+                    iterations: u32) -> (f64, f64) {
+    let mut m_current = m;
+    let mut b_current = b;
     let n = points_x.len() as f64;
+    let inv_n = 1.0 / n;
 
-    // Compute partial derivatives with respect to m and b, respectively
-    for (x, y) in points_x.iter().zip(points_y.iter()) {
-        m_partial += -(2.0 / n) * (y - (m * x + b));
-        b_partial += (2.0 / n) * x * (y - (m * x + b));
+    // Perform gradient descent i times
+    for i in 0..iterations {
+        // Compute the partial derivatives of the MSE cost function with
+        // respect to m and b
+        let mut m_partial = 0.0;
+        let mut b_partial = 0.0;
+        for (x, y) in points_x.iter().zip(points_y.iter()) {
+            m_partial += ((m_current * x + b_current) - y) * x;
+            b_partial += ((m_current * x + b_current) - y);
+        }
+        m_partial *= inv_n;
+        b_partial *= inv_n;
+
+        // Update the current estimates of m and b: the gradient points in
+        // the direction of increase, which is why we subtract it from the
+        // current estimates
+        m_current -= learning_rate * m_partial;
+        b_current -= learning_rate * b_partial;
     }
-
-    // Update m and b
-    (m - learning_rate * m_partial, b - learning_rate * b_partial)
+        (m_current, b_current)
 }
 
 /// Computes the mean squared error
@@ -35,7 +51,6 @@ fn mse(m: f64,
         // Find the squared distance between the point on the line corresponding
         // to x and the actual value of y
         calculated_error += (y - (m * x + b)).powf(2.0);
-        println!("{}, {} - accumulated error: {}", x, y, calculated_error);
     }
     calculated_error / points_x.len() as f64
 }
@@ -47,20 +62,17 @@ fn run(points_x: &[f64], points_y: &[f64]) {
     let m_initial = 0.0;
     let b_initial = 0.0;
     let learning_rate = 0.0001;
-    let iterations = 1;
+    let iterations = 1000;
 
     // (3) Train the model
     let error_initial = mse(m_initial, b_initial, points_x, points_y);
     println!("Starting gradient descent...");
     println!("Initially, m = {}, b = {}, error = {}", m_initial, b_initial, error_initial);
-    let mut m_current = m_initial;
-    let mut b_current = b_initial;
-    for i in 0..iterations {
-        (m_current, b_current) = gradient_descent(m_initial, b_initial, points_x, points_y, learning_rate);
-    }
 
-    let error_final = mse(m_current, b_current, points_x, points_y);
-    println!("After {} iterations,  m = {}, b = {}, error = {}", iterations, m_current, b_current, error_final);
+    let (m_final, b_final) = gradient_descent(m_initial, b_initial, points_x, points_y, learning_rate, iterations);
+    let error_final = mse(m_final, b_final, points_x, points_y);
+
+    println!("After {} iterations,  m = {}, b = {}, error = {}", iterations, m_final, b_final, error_final);
 }
 
 fn main() {
